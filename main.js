920c1a7a092e5a99c24fe4c50d62160f94656cd1
Ranto Bastara Hamonangan Sitorus <rantositorus44@gmail.com>
2024-12-09 18:23:20 +0700

    feat: add alphabet and digit

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 import * as THREE from "three";
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
 
+// Scene, Camera, Renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -7,22 +10,42 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
 
 camera.position.z = 5;
 
-function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+const lastAlphabet = "o";
+const lastDigit = "8";
 
+const fontLoader = new FontLoader();
+fontLoader.load(
+  "https://threejs.org/examples/fonts/helvetiker_regular.typeface.json",
+  (font) => {
+    const alphabetGeometry = new TextGeometry(lastAlphabet, {
+      font: font,
+      size: 1,
+      height: 0.2,
+    });
+    const alphabetMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    const alphabetMesh = new THREE.Mesh(alphabetGeometry, alphabetMaterial);
+    alphabetMesh.position.set(-2, 0, 0);
+    scene.add(alphabetMesh);
+
+    const digitGeometry = new TextGeometry(lastDigit, {
+      font: font,
+      size: 1,
+      height: 0.2,
+    });
+    const digitMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+    const digitMesh = new THREE.Mesh(digitGeometry, digitMaterial);
+    digitMesh.position.set(2, 0, 0);
+    scene.add(digitMesh);
+  }
+);
+
+function animate() {
   renderer.render(scene, camera);
 }
+renderer.setAnimationLoop(animate);
